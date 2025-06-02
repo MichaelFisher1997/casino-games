207ee345ee6a724491bfc8bda86f7c2e5f239e62
MichaelFisher1997 <contact@micahelfisher.tech>
2025-06-02 01:39:34 +0100

    FIX: Zip path

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,33 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# --- IAM Role and Profile for Beanstalk EC2 instances ---
+resource "aws_iam_role" "beanstalk_ec2_role" {
+  name = "aws-elasticbeanstalk-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "beanstalk_ec2_policy" {
+  role       = aws_iam_role.beanstalk_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+}
+
+resource "aws_iam_instance_profile" "beanstalk_ec2_profile" {
+  name = "aws-elasticbeanstalk-ec2-role"
+  role = aws_iam_role.beanstalk_ec2_role.name
+}
+
+# --- Elastic Beanstalk Application and App Version ---
 resource "aws_elastic_beanstalk_application" "slot_gui_app" {
   name        = "slot-gui-app"
   description = "Elastic Beanstalk application for slot-gui Node app"
@@ -14,10 +41,11 @@ resource "aws_elastic_beanstalk_application_version" "init" {
   key         = "app.zip"
 }
 
+# --- Elastic Beanstalk Environment ---
 resource "aws_elastic_beanstalk_environment" "slot_gui_env" {
   name                = "slot-gui-env"
   application         = aws_elastic_beanstalk_application.slot_gui_app.name
-  solution_stack_name = "64bit Amazon Linux 2023 v6.5.2 running Node.js 18"
+  solution_stack_name = "64bit Amazon Linux 2023 v6.5.2 running Node.js 22"
   version_label       = aws_elastic_beanstalk_application_version.init.name
 
   setting {
@@ -25,9 +53,21 @@ resource "aws_elastic_beanstalk_environment" "slot_gui_env" {
     name      = "EnvironmentType"
     value     = "SingleInstance"
   }
+
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "InstanceType"
     value     = "t3.micro"
   }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.beanstalk_ec2_profile.name
+  }
 }
+
+output "beanstalk_environment_url" {
+  value = aws_elastic_beanstalk_environment.slot_gui_env.endpoint_url
+}
+
--- terraform/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "beanstalk_environment_url" {
-  description = "Elastic Beanstalk environment endpoint URL"
-  value       = aws_elastic_beanstalk_environment.slot_gui_env.endpoint_url
-}
-
