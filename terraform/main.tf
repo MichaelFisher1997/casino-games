980d87451aaee4b05cc2deb50d2b55105c2d9602
MichaelFisher1997 <contact@micahelfisher.tech>
2025-06-02 02:50:53 +0100

    FIX: Bun build

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,7 +45,7 @@ resource "aws_elastic_beanstalk_application_version" "init" {
 resource "aws_elastic_beanstalk_environment" "slot_gui_env" {
   name                = "slot-gui-env"
   application         = aws_elastic_beanstalk_application.slot_gui_app.name
-  solution_stack_name = "64bit Amazon Linux 2023 v6.5.2 running Node.js 22"
+  solution_stack_name = "64bit Amazon Linux 2023 v6.5.2 running Docker"
   version_label       = aws_elastic_beanstalk_application_version.init.name
 
   setting {
