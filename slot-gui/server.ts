4b7b4d9ae6bbd7f29a8730abee9fac5bee9e3f9d
MichaelFisher1997 <contact@micahelfisher.tech>
2025-06-08 10:37:26 +0100

    coolidy

--- slot-gui/server.ts
+++ slot-gui/server.ts
@@ -10,7 +10,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9090;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
